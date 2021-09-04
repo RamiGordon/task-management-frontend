6c16673a230f59e104a93bf7216ec0d93bb037f2
RamiGordon <RamiGordon@users.noreply.github.com>
2021-09-04 22:25:24 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.1da67bc7478b9bd0900ab01eb741fec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d45ac8672687dfce32793bd235e635",
+    "url": "https://RamiGordon.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "cadb774a60492a326ee9",
+    "url": "https://RamiGordon.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "d581b4650d878da7be44",
+    "url": "https://RamiGordon.github.io/task-management-frontend/static/js/2.4566ac37.chunk.js"
+  },
+  {
+    "revision": "cadb774a60492a326ee9",
+    "url": "https://RamiGordon.github.io/task-management-frontend/static/js/main.12e84216.chunk.js"
+  },
+  {
+    "revision": "e97a66473cf9bdfb2455",
+    "url": "https://RamiGordon.github.io/task-management-frontend/static/js/runtime~main.ce90c953.js"
+  }
+]);
